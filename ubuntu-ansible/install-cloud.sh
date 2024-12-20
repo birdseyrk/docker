1e5c3f3a4bbf7ee277a0f64e7ae4972540ea70a8
birdseyrk <birdseyrk@gmail.com>
2024-12-20 14:18:15 -0700

    Initial Delivery

--- ubuntu-ansible/install-cloud.sh
+++ ubuntu-ansible/install-cloud.sh
@@ -1,4 +1,5 @@
-docker build --no-cache --add-host ansible-master -t ansible-master . -f Dockerfile-ansible-master
+#docker build --no-cache --add-host ansible-master:172.20.0.20 -t ansible-master . -f Dockerfile-ansible-master
+docker build --no-cache -t ansible-master . -f Dockerfile-ansible-master
 
 docker build --no-cache -t ubuntu-node . -f Dockerfile-ansible-node
 
