ff8a04e82e04af4f37bc6b539877fcc19c45dd57
birdseyrk <birdseyrk@gmail.com>
2024-12-21 15:16:24 -0700

    Initial Delivery

--- ubuntu-ansible/install-cloud-master.sh
+++ ubuntu-ansible/install-cloud-master.sh
@@ -2,4 +2,4 @@
 
 docker run -dt -h ansible-master --net ansiblenet --ip 172.20.0.20 --restart=unless-stopped --name ansible-master ansible-master
 docker exec -t ansible-master /bin/bash -c "/setup/setup-master-hostfile.sh"
-docker exec -t -u ansadmin ansible-master /bin/bash -c "/setup/setup-ansadmin.sh"
+docker exec -t -u ansadmin ansible-master /bin/bash -c "/setup/setup-ansadmin-hostfile.sh"
