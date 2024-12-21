511f92a4b395bfe34e0268e82be270d770383d07
birdseyrk <birdseyrk@gmail.com>
2024-12-21 14:16:17 -0700

    Initial Delivery

--- ubuntu-ansible/restart-ssh-service.sh
+++ ubuntu-ansible/restart-ssh-service.sh
@@ -6,4 +6,4 @@ echo "restart-ssh-service.sh [ $1 ]" >> /var/log/ops.log
 
 #docker exec -t $1 /bin/bash -c  "service ssh restart"
 
-/usr/bin/ansible-playbook -i /home/ansadmin/local/docker-ubuntu/docker/ubuntu-ansible/restart-inventory.txt /home/ansadmin/local/docker-ubuntu/docker/ubuntu-ansible/restart-ssh-service.yaml --extra-vars "myHost='$1'"
+/usr/bin/ansible-playbook -i /ops/ansible/restart-inventory.txt /ops/ansible/restart-ssh-service.yaml --extra-vars "myHost='$1'"
