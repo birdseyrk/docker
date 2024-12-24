bf7f507b8b1c115ed19e329a0e1b5bcde5997bc1
birdseyrk <birdseyrk@gmail.com>
2024-12-24 10:26:31 -0700

    Initial Delivery

--- ubuntu-ansible/setup-cloud-master.sh
+++ ubuntu-ansible/setup-cloud-master.sh
@@ -2,6 +2,5 @@
 
 #run on ubuntu2
 
-#docker exec -t ansible-master /bin/bash -c "/setup/setup-master-hostfile.sh"
 docker exec -t -u ansadmin ansible-master /bin/bash -c "/setup/setup-ansadmin.sh"
 docker exec -t -u ansadmin ansible-master /bin/bash -c "/setup/setup-ssh-keys.sh"
